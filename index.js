98419be19a32a20f30b19b3726b483b2dbba8795
Sachin Mahajan <sachinm78@gmail.com>
2019-06-12 19:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,12 @@ const driversByName = function (drivers) {
   })
 }
 
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total
+  }, 0)
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
